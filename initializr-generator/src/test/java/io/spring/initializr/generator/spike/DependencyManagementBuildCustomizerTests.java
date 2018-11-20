34388a99bd4bb9400bbfaa6f2faea1b76e6e7d28
Stephane Nicoll <snicoll@pivotal.io>
2018-11-20 11:03:33 +0100

    Switch project structure to use the new generator

---
+++ initializr-generator/src/test/java/io/spring/initializr/generator/spike/DependencyManagementBuildCustomizerTests.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.initializr.generator.spike;
+
+import io.spring.initializr.generator.ProjectDescription;
+import io.spring.initializr.generator.buildsystem.Build;
+import io.spring.initializr.generator.buildsystem.maven.MavenBuild;
+import io.spring.initializr.generator.spike.build.DependencyManagementBuildCustomizer;
+import io.spring.initializr.generator.util.Version;
+import io.spring.initializr.metadata.BillOfMaterials;
+import io.spring.initializr.metadata.Dependency;
+import io.spring.initializr.metadata.InitializrMetadata;
+import io.spring.initializr.test.metadata.InitializrMetadataTestBuilder;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link DependencyManagementBuildCustomizer}.
+ *
+ * @author Stephane Nicoll
+ */
+public class DependencyManagementBuildCustomizerTests {
+
+	@Test
+	public void contributeBom() { // ProjectRequestTests#resolveAdditionalBoms
+		Dependency dependency = Dependency.withId("foo");
+		dependency.setBom("foo-bom");
+		BillOfMaterials bom = BillOfMaterials.create("com.example", "foo-bom", "1.0.0");
+		bom.getAdditionalBoms().add("bar-bom");
+		BillOfMaterials additionalBom = BillOfMaterials.create("com.example", "bar-bom",
+				"1.1.0");
+		InitializrMetadata metadata = InitializrMetadataTestBuilder.withDefaults()
+				.addBom("foo-bom", bom).addBom("bar-bom", additionalBom)
+				.addDependencyGroup("test", dependency).build();
+		Build build = new MavenBuild();
+		build.addDependency(dependency.getId(),
+				ConceptTranslator.toDependency(dependency));
+		customizeBuild(build, metadata);
+		assertThat(build.getBoms()).hasSize(2);
+	}
+
+	@Test
+	public void contributeRepositories() { // ProjectRequestTests#resolveAdditionalRepositories
+		Dependency dependency = Dependency.withId("foo");
+		dependency.setBom("foo-bom");
+		dependency.setRepository("foo-repo");
+		BillOfMaterials bom = BillOfMaterials.create("com.example", "foo-bom", "1.0.0");
+		bom.getRepositories().add("bar-repo");
+		InitializrMetadata metadata = InitializrMetadataTestBuilder.withDefaults()
+				.addBom("foo-bom", bom)
+				.addRepository("foo-repo", "foo-repo", "http://example.com/foo", false)
+				.addRepository("bar-repo", "bar-repo", "http://example.com/bar", false)
+				.addDependencyGroup("test", dependency).build();
+		Build build = new MavenBuild();
+		build.addDependency(dependency.getId(),
+				ConceptTranslator.toDependency(dependency));
+		customizeBuild(build, metadata);
+		assertThat(build.getRepositories()).hasSize(2);
+		assertThat(build.getPluginRepositories()).isEmpty();
+	}
+
+	private void customizeBuild(Build build, InitializrMetadata metadata) {
+		ProjectDescription projectDescription = new ProjectDescription();
+		projectDescription.setPlatformVersion(Version.parse("2.0.0.RELEASE"));
+		new DependencyManagementBuildCustomizer(projectDescription, metadata)
+				.customize(build);
+	}
+
+}
