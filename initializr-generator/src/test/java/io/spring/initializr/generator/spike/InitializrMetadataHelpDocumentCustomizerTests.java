f85d0fb153377df6056efdb39e3b57e45f8dbc36
Stephane Nicoll <snicoll@pivotal.io>
2019-01-08 15:23:57 +0100

    Polish

--- initializr-generator/src/test/java/io/spring/initializr/generator/spike/InitializrMetadataHelpDocumentCustomizerTests.java
+++ initializr-generator/src/test/java/io/spring/initializr/generator/spike/InitializrMetadataHelpDocumentCustomizerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package io.spring.initializr.generator.spike;
 
 import io.spring.initializr.generator.ProjectDescription;
-import io.spring.initializr.generator.ResolvedProjectDescription;
 import io.spring.initializr.generator.project.documentation.HelpDocument;
 import io.spring.initializr.generator.spike.documentation.InitializrMetadataHelpDocumentCustomizer;
 import io.spring.initializr.generator.util.Version;
@@ -80,8 +79,8 @@ public class InitializrMetadataHelpDocumentCustomizerTests {
 	private HelpDocument contributeHelpDocument(ProjectDescription projectDescription,
 			InitializrMetadata metadata) {
 		HelpDocument document = new HelpDocument(new MustacheTemplateRenderer(""));
-		new InitializrMetadataHelpDocumentCustomizer(projectDescription.resolve(), metadata)
-				.customize(document);
+		new InitializrMetadataHelpDocumentCustomizer(projectDescription.resolve(),
+				metadata).customize(document);
 		return document;
 	}
 
